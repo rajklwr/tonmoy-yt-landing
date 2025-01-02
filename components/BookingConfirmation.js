ffd15e12f07623d497df96d99c3cb0c5e490ad14
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2025-01-02 12:43:05 +0530

    'thank-you'

---
+++ components/BookingConfirmation.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BookingConfirmation = () => {
+  return (
+    <div className=" min-h-screen "
+    style={{
+        backgroundImage: 'url("/images/bg2.png")', // Ensure this is the correct path to your background
+        backgroundAttachment: 'fixed', // Makes the background image stay fixed
+      }}
+    >
+<div className="absolute "> 
+
+</div>
+    </div>
+  );
+};
+
+export default BookingConfirmation;
