a49bf79523092c5ba39be523614ce24c444bff56
Rajkiran Kalowar <rajkiranjnv1@gmail.com>
2025-02-05 22:32:31 +0530

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: "https://www.yt-growthsecrets.com", 
+    generateRobotsTxt: true, // Generates robots.txt file
+    sitemapSize: 7000, // Optional: Limits URLs per sitemap file
+  };
+  
